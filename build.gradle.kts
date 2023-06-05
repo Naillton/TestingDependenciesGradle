8dd984488c4d889131b3bed062118b50738ce7ff
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-06-05 13:24:57 -0300

    spring dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// criando dependencias com o gradle, para conseguir criar via linha de comando usamos o gradle init e preenchemos todas as informacoes
+// com a ide do intellij so precisamos escolher o pacote de dependencia e criar o projeto, logo apos iniciar a ide o gradle fara downloads
 plugins {
     id("java")
 }
@@ -9,9 +11,11 @@ repositories {
     mavenCentral()
 }
 
+// colocando dependencia do spring no gradle, visitar site de repositorios centrais do maven
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
 }
 
 tasks.test {
